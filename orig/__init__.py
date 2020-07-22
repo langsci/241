5bcb7104a84594d5a9f02d1aa2b94d9820859047
Felix Kopecky <kopeckyf@users.noreply.github.com>
2020-07-22 18:08:36 +0200

    rm tmp python files

--- orig/__init__.py
+++
@@ -1,6 +0,0 @@
-name = "langsci"
-
-#__all__ = ['asciify', 'assignproofreaders', 'autoindex', 'bibnouns',
-           #'delatex', 'doc2tex', 'extractaw','fixindex','langscibibtex','normalizebib','sanitycheck','sanitygit','zenodo']
-           
- 
